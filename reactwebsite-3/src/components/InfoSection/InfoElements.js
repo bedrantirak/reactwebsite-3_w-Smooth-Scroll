a2e39a8371c878333e39c9bf169a7b82b21c792a
bedrantirak <bdrntrk@gmail.com>
2020-10-25 23:20:23 +0300

    Update InfoElements.js

--- reactwebsite-3/src/components/InfoSection/InfoElements.js
+++ reactwebsite-3/src/components/InfoSection/InfoElements.js
@@ -63,7 +63,8 @@ margin-bottom: 16px;
 export const Heading = styled.h1`
 margin-bottom: 24px;
 font-size: 48px;
-line-height:  680;
+line-height: 1.1;
+font-weight: 600;
 color: ${({lightText})=>(lightText ? "#f7f8fa":"#010606")};
 
 @media screen and (max-width: 480px){
