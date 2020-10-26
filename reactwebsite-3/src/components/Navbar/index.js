6d86e792387f6410c7b53748366fefe15127dc0c
bedrantirak <bdrntrk@gmail.com>
2020-10-26 19:51:12 +0300

    Update index.js

--- reactwebsite-3/src/components/Navbar/index.js
+++ reactwebsite-3/src/components/Navbar/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import {FaBars} from "react-icons/fa"
+import {IconContext} from "react-icons/lib"
 import { Nav,
     NavbarContainer,
     NavLogo,
@@ -13,9 +14,24 @@ import { Nav,
 
 
 const Navbar = ({toggle}) => {
+    
+    const[scrollNav, setScrollNav] = useState(false)
+
+    const changeNav = ()=>{
+        if(window.scrollY >= 80){
+            setScrollNav(true)
+        } else{
+            setScrollNav(false)
+        }
+    }
+    useEffect(() => {
+        window.addEventListener("scroll",changeNav)
+    }, [])
+
     return (
         <>
-        <Nav>
+        <IconContext.Provider value={{color: "#fff"}}>
+        <Nav scrollNav={scrollNav}>
             <NavbarContainer>
                 <NavLogo to="/">bdrn</NavLogo>
                 <MobileIcon onClick={toggle} >
@@ -40,6 +56,7 @@ const Navbar = ({toggle}) => {
                 </NavBtn>
             </NavbarContainer>
         </Nav>
+        </IconContext.Provider>
         </>
     )
 }
